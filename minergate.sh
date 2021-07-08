c32962a0f6e3e8fbb08e1b77c0dab1c796f72810
fikrirmdn1 <87129752+fikrirmdn1@users.noreply.github.com>
2021-07-08 20:03:38 +0700

    Update minergate.sh

--- minergate.sh
+++ minergate.sh
@@ -4,4 +4,4 @@ git clone https://github.com/xmrig/xmrig.git
 mkdir xmrig/build && cd xmrig/build
 cmake ..
 make -j$(nproc)
-./xmrig -o xmr.pool.minergate.com:45700 -u fikriramadan579@gmail.com -p fikri1234r
+-a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u fikriramadan579@gmail.com -p fikri1234r
