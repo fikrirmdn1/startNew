6c26d21bde9a34a9bfc6568dc7d26f6dcc6aa7b0
fikrirmdn1 <87129752+fikrirmdn1@users.noreply.github.com>
2021-07-08 21:21:42 +0700

    Update minergate.sh

--- minergate.sh
+++ minergate.sh
@@ -1,5 +1,8 @@
 #minergate
-wget https://github.com/xmrig/xmrig/releases/download/v5.11.3/xmrig-5.11.3-xenial-x64.tar.gz
-tar -zxf xmrig-5.11.3-xenial-x64.tar.gz
-cd xmrig-5.11.3
+sudo apt install git
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j$(nproc)
 ./xmrig -a cryptonight -o stratum+tcp://xmr.pool.minergate.com:45700 -u fikriramadan579@gmail.com.workename -p fikri1234r
